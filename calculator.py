63a198c58090858ecc0be966167fa1b1e82c4b2d
startop1116 <hyeonho@baghyeonhoui-MacBookPro.local>
2022-08-01 21:11:56 +0900

    Add def Average to  Caculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def reminder(a,b):
+    return a//b
+
+def average(a,b):
+    return (a+b)/2
